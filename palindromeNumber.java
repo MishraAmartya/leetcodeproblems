20b0b1440dc072bf54f2a16ff7e7d3d5fde6bdd7
kumar amartya <kumaramartya490@gmail.com>
2026-03-22 15:53:36 +0530

    Add files via upload

---
+++ palindromeNumber.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+
+        if(x<0){
+            return false;
+        }
+
+        int rev = 0;
+        int org = x;
+        while(x!=0){
+        int lastDigit = x%10;
+        rev = rev*10 + lastDigit;
+        x = x/10;
+        }
+        return (rev == org);
+
+        
+    }
+}
